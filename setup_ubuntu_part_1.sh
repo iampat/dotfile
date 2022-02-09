b014d8f5671bfb1a6162445a5b761778fabb43c3
Ali Amiri <ali@amiri.dev>
2022-02-09 12:01:41 -0800

    comment active commands

--- setup_ubuntu_part_1.sh
+++ setup_ubuntu_part_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## install chrome
+# # install chrome
 # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 # sudo dpkg -i google-chrome-stable_current_amd64.deb
 
@@ -9,7 +9,7 @@
 
 # sudo apt-get install emacs xclip tmux
 
-## docker https://docs.docker.com/engine/install/ubuntu/
+# # docker https://docs.docker.com/engine/install/ubuntu/
 # sudo apt-get remove docker docker-engine docker.io containerd runc
 
 # sudo apt-get install ca-certificates curl gnupg lsb-release
@@ -20,30 +20,30 @@
 #  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # sudo apt-get update
 # sudo apt-get install docker-ce docker-ce-cli containerd.io
-## post docker install https://docs.docker.com/engine/install/linux-postinstall/
+# # post docker install https://docs.docker.com/engine/install/linux-postinstall/
 # sudo groupadd docker
 # sudo usermod -aG docker $USER
 
-## google cloud sdk https://cloud.google.com/sdk/docs/install#deb
+# # google cloud sdk https://cloud.google.com/sdk/docs/install#deb
 # sudo apt-get install apt-transport-https ca-certificates gnupg
 # echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 # curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 # sudo apt-get update
 # sudo apt-get install google-cloud-sdk
 
-# install bazel
-sudo apt install apt-transport-https curl gnupg
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
-sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-# we are using bazel 4.0.0 https://docs.bazel.build/versions/main/install-ubuntu.html
-sudo apt update && sudo apt install bazel-4.0.0
-sudo ln -s /usr/bin/bazel-4.0.0 /usr/bin/bazel
-
-## install slack
+# # install bazel
+# sudo apt install apt-transport-https curl gnupg
+# curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
+# sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
+# echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+# # we are using bazel 4.0.0 https://docs.bazel.build/versions/main/install-ubuntu.html
+# sudo apt update && sudo apt install bazel-4.0.0
+# sudo ln -s /usr/bin/bazel-4.0.0 /usr/bin/bazel
+# 
+# # install slack
 # sudo snap install slack --classic
 
-## install vs code https://code.visualstudio.com/docs/setup/linux
+# # install vs code https://code.visualstudio.com/docs/setup/linux
 # wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 # sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 # sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
