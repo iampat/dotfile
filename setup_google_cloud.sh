2a580c4d7bac61835ee913ca8aebe9212d0d721f
Ali Amiri <ali@amiri.dev>
2022-09-08 23:30:03 -0700

    update september 8

--- setup_google_cloud.sh
+++ setup_google_cloud.sh
@@ -3,10 +3,11 @@ set -e
 
 cd /tmp
 
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-377.0.0-linux-x86_64.tar.gz
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-384.0.1-linux-x86_64.tar.gz
+       
 install_path=${HOME}/.local/bin/
 mkdir -p ${install_path}
-tar -xf google-cloud-sdk-377.0.0-linux-x86_64.tar.gz -C ${install_path}
+tar -xf google-cloud-cli-384.0.1-linux-x86.tar.gz -C ${install_path}
 
 ${install_path}/google-cloud-sdk/install.sh
  
